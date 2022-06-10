69def91a12b417855ce9c1f5fdf8ef450ca4119a
Amay <brajbliss@gmail.com>
2022-06-10 13:20:46 +0530

    fetch and display

--- src/App.js
+++ src/App.js
@@ -1,1 +1,23 @@
 import './App.css';
+
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const App = () => {
+	const [advice, setAdvice] = useState(' ');
+
+	useEffect(() => {
+		axios.get('https://api.adviceslip.com/advice')
+			.then((response) => {
+				const { advice } = response.data.slip;
+				setAdvice(advice);
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	}, []);
+
+	return <h1>{advice}</h1>;
+};
+
+export default App;
