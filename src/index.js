9e134e991b30cabb8ae4802457344c91b92f7fa2
Amay <brajbliss@gmail.com>
2022-06-10 11:44:36 +0530

    initializing project with React v18

---
+++ src/index.js
@@ -0,0 +1,17 @@
+// Before React v18
+// import React from 'react';
+// import { ReactDOM } from 'react-dom';
+
+// import App from './App';
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+// From React v18
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+import App from './App';
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App />);
